0f915702fa792ce23f9a554bb8b7535044b77d20
Endaethia <fail.community@hotmail.com>
2024-01-26 13:36:55 +0200

    Project Done

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,53 @@
 #!/bin/bash
 
+#EXPORT KEY PATH
+#KEY_PATH="/home/tamer/Desktop/TamerJjeney-KeyPair.pem"
+########################################################
 
+if [[  -z ${KEY_PATH} ]]
+
+then
+
+        echo "KEY_PATH env var is expected"
+
+        exit 5
+
+fi
+
+########################################################
+USER="ubuntu"
+# Connect to Public Instance
+
+if [[ $# == 1 ]]
+
+then
+
+        ssh -i $KEY_PATH "$USER@$1"
+
+fi
+
+#####
+
+##Start the Agent and add the Key
+eval $(ssh-agent -s)
+ssh-add $KEY_PATH
+# Conenct to Private using Public: ( must have two argument when:)
+if [[ $# == 2 ]]
+
+then
+
+        ssh -A  -o StrictHostKeyChecking=no -i "$KEY_PATH" -J "$USER@$1" "$USER@$2"
+
+fi
+
+## Add an optional command
+
+if [[ $# == 3 ]]
+
+then
+
+        ssh -A  -o StrictHostKeyChecking=no -i "$KEY_PATH" -J "$USER@$1" "$USER@$2" $3
+
+fi
+###########################################################################################
 # TODO your solution here
